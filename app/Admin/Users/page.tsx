bbdf1714c5b9b7e0fbc58068219b157162a07ad6
Ploydy <Lfecarballo20@gmail.com>
2023-11-26 19:03:48 +0800

    Register page updated ui

--- app/Admin/Users/page.tsx
+++ app/Admin/Users/page.tsx
@@ -1,63 +1,84 @@
-import { Button } from '@nextui-org/react'
-import Link from 'next/link'
-import React from 'react'
+"use client";
+
+import { Button } from "@nextui-org/react";
+import Link from "next/link";
+import React from "react";
 
 const Users = () => {
+  const handleAddUser = () => {
+    alert("Add user");
+  };
+  const handleDelete = () => {
+    alert("Delete user");
+  };
+
+  const handleUpdate = () => {
+    alert("Update user");
+  };
+
   return (
     <div>
       <>
-      <div>
-        <h3>User</h3>
-      </div>
-      <button className="btn">Add a new user</button>
-      <table>
-        <thead>
-          <tr>
-            <th className="border w-1/5 px-1 py-2" scope="col">
-              Id
-            </th>
-            <th className="border w-1/5 px-1 py-2" scope="col">
-              User
-            </th>
-            <th className="border w-1/5 px-1 py-2" scope="col">
-              email
-            </th>
-            <th className="border w-1/5 px-1 py-2" scope="col">
-              password
-            </th>
-            <th className="border w-1/5 px-1 py-2" scope="col">
-              Actions
-            </th>
-          </tr>
-        </thead>
+        <div>
+          <h3>User</h3>
+        </div>
+        <Button className="btn my-4" onClick={handleAddUser}>
+          Add user
+        </Button>
+        <table>
+          <thead>
+            <tr>
+              <th className="border w-1/5 px-1 py-2" scope="col">
+                Id
+              </th>
+              <th className="border w-1/5 px-1 py-2" scope="col">
+                User
+              </th>
+              <th className="border w-1/5 px-1 py-2" scope="col">
+                email
+              </th>
+              <th className="border w-1/5 px-1 py-2" scope="col">
+                password
+              </th>
+              <th className="border w-1/5 px-1 py-2" scope="col">
+                Actions
+              </th>
+            </tr>
+          </thead>
 
-        <tbody>
-          <tr>
-            <th className="border px-4 py-2" scope="row">
-              User id
-            </th>
-            <td className="border px-4 py-2 text-center">User Fullname</td>
-            <td className="border px-4 py-2 text-center">User Email</td>
-            <td className="border px-4 py-2 text-center">User Password</td>
-            <td className="border px-4 py-2 text-center">
-              <Link href="/Courses">
-                <Button className="btn bg-green-400">Update</Button>
-              </Link>
-              <Button
-              className="btn bg-red-500"
-                color="danger"
-                variant="bordered"
-                /* onClick={() => handleDelete(user.id)} */
-              >
-                Delete
-              </Button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </>
+          <tbody>
+            <tr>
+              <th className="border px-4 py-2" scope="row">
+                User id
+              </th>
+              <td className="border px-4 py-2 text-center">User Fullname</td>
+              <td className="border px-4 py-2 text-center">User Email</td>
+              <td className="border px-4 py-2 text-center">User Password</td>
+              <td className="border px-4 py-2 text-center">
+                <Link href="/Admin/Users">
+                  <Button
+                    className="btn bg-green-400 mx-2"
+                    onClick={handleUpdate}
+                  >
+                    Update
+                  </Button>
+                </Link>
+                <Button
+                  className="text-slate-950 capitalize bg-red-500"
+                  color="danger"
+                  variant="bordered"
+                  onClick={handleDelete}
+                  /* onClick={() => handleDelete(user.id)} */
+                >
+                  Delete
+                </Button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </>
     </div>
-  )
-}
+  );
+};
 
-export default Users
+export default Users;
--- app/Register/page.tsx
+++ app/Register/page.tsx
@@ -2,13 +2,16 @@
 
 import React, { useMemo, useState } from "react";
 import Image from "next/image";
-import BgImage from "./../(images)/logo.jpg";
-import { Button, Card, CardBody, Checkbox, Input } from "@nextui-org/react";
+
+import BgImage from "../(images)/logo2.png";
 import {
-  Dropdown,
-  DropdownTrigger,
-  DropdownMenu,
-  DropdownItem,
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Checkbox,
+  CheckboxGroup,
+  Input,
 } from "@nextui-org/react";
 import Link from "next/link";
 
@@ -19,118 +22,130 @@ const Register = () => {
     [selectedKeys]
   );
   const handleRegister = () => {
+    alert("Registered Successfully");
     console.log("Register");
+  };
+  const handleEmailChecker = () => {
+    alert("Email checker approved");
+  };
+  const handleUploadImage = () => {
+    alert("Image Uploaded ");
   };
 
   return (
     <>
-      <div>
-        <div
-          style={{
-            zIndex: -1,
-            position: "fixed",
-            width: "100vw",
-            height: "100vh",
-          }}
-        >
-          <Image
-            src={BgImage}
-            alt="Background Image"
-            fill
-            style={{ objectFit: "cover" }}
-          />
-        </div>
+      <div
+        style={{
+          zIndex: -1,
+          position: "fixed",
+          width: "100vw",
+          height: "100vh",
+        }}
+      >
+        <Image
+          src={BgImage}
+          alt="Background Image"
+          fill
+          style={{ objectFit: "cover" }}
+        />
+      </div>
 
-        <div className="px-4 py-40 mx-auto sm:px-6 lg:px-8 max-w-7x1">
-          <div className="max-w-lg mx-auto text-center">
-            <Card
-              className="pt-4 pb-4 px-8 bg-transparent"
-              style={{ border: "solid" }}
-            >
-              <h1 className="text-tiny uppercase font-bold">Login</h1>
-              <CardBody>
-                <Input
-                  className="py-1"
-                  type="email"
-                  name="email"
-                  placeholder="Email"
-                />
-                <Input
-                  className="py-1"
-                  type="email"
-                  name="email"
-                  placeholder="Confirm Email"
-                />
+      <div className="px-4 py-40 mx-auto sm:px-6 lg:px-8 max-w-7x1">
+        <div className="max-w-lg mx-auto text-center">
+          <Card
+            className="pt-4 pb-4 px-8 bg-transparent"
+            style={{ border: "solid" }}
+          >
+            <h1 className="text-tiny uppercase font-bold">Register</h1>
+            <CardBody>
+              <Input
+                className="py-1"
+                type="email"
+                name="email"
+                placeholder="Email"
+              />
+              <Input
+                className="py-1"
+                type="email"
+                name="email"
+                placeholder="Confirm Email"
+              />
 
-                <Button>Check Email</Button>
+              <Button className="my-4 mx-10" onClick={handleEmailChecker}>
+                Check Email
+              </Button>
 
-                <Input
-                  className="py-1"
-                  type="text"
-                  name="surName"
-                  placeholder="Surname"
-                />
-                <Input
-                  className="py-1"
-                  type="text"
-                  name="firstName"
-                  placeholder="First Name"
-                />
-                <Input
-                  className="py-1"
-                  type="text"
-                  name="middleName"
-                  placeholder="Middle Name"
-                />
-                <Input
-                  className="py-1"
-                  type="date"
-                  name="birthday"
-                  placeholder="Birth-Day"
-                />
-                <Input
-                  className="py-1"
-                  type="number"
-                  name="mobileNumber"
-                  placeholder="Mobile No."
-                />
-                <Input
-                  className="py-1"
-                  type="address"
-                  name="Address"
-                  placeholder="Address"
-                />
+              <Input
+                className="py-1"
+                type="text"
+                name="surName"
+                placeholder="Surname"
+              />
+              <Input
+                className="py-1"
+                type="text"
+                name="firstName"
+                placeholder="First Name"
+              />
+              <Input
+                className="py-1"
+                type="text"
+                name="middleName"
+                placeholder="Middle Name"
+              />
+              <Input
+                className="py-1"
+                type="date"
+                name="birthday"
+                placeholder="Birth-Day"
+              />
+              <Input
+                className="py-1"
+                type="number"
+                name="mobileNumber"
+                placeholder="Mobile No."
+              />
+              <Input
+                className="py-1"
+                type="address"
+                name="Address"
+                placeholder="Address"
+              />
 
-                <div className="py-14">
-                  Picture
-                </div>
+              <div className="py-14 ">
+                <Card className="py-4 bg-transparent">
+                  <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+                    <p className="text-tiny uppercase font-bold"></p>
+                    <small className="text-default-500"></small>
+                    <h4 className="font-bold text-large">Profile Picture</h4>
+                  </CardHeader>
+                  <CardBody className="overflow-visible py-2"></CardBody>
+                </Card>
+                <Button onClick={handleUploadImage}>Upload Image</Button>
+              </div>
 
+              <Input
+                className="py-1 "
+                type="password"
+                name="password"
+                placeholder="Password"
+              />
+              <Input
+                className="py-1 "
+                type="password"
+                name="confirm-password"
+                placeholder="Confirm Password"
+              />
 
+              <div className="my-2">
+                <Link href="/">
+                  <Button>Already have an account?</Button>
+                </Link>
+              </div>
 
-
-                <Input
-                  className="py-1 "
-                  type="password"
-                  name="password"
-                  placeholder="Password"
-                />
-                <Input
-                  className="py-1 "
-                  type="password"
-                  name="confirm-password"
-                  placeholder="Confirm Password"
-                />
-
-                <div className="my-2">
-                  <Link href="/">
-                    <Button>Already have an account?</Button>
-                  </Link>
-                </div>
-
-                <Button onClick={handleRegister}>Register</Button>
-              </CardBody>
-            </Card>
-          </div>
+              <Button onClick={handleRegister}>Register</Button>
+            </CardBody>
+          </Card>
         </div>
       </div>
     </>
